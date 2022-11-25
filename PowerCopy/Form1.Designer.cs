2e57f55af1848f8745aa9c734deae7d05aec08f6
Daryn Liebenberg <daryn7lieb@gmail.com>
2022-11-25 17:02:07 +0200

    Add project files.

---
+++ PowerCopy/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace PowerCopy
+{
+    partial class PowerCopyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.SourceButton = new System.Windows.Forms.Button();
+            this.DestinationButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.CopyButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog
+            // 
+            this.folderBrowserDialog.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // SourceButton
+            // 
+            this.SourceButton.Location = new System.Drawing.Point(24, 21);
+            this.SourceButton.Name = "SourceButton";
+            this.SourceButton.Size = new System.Drawing.Size(75, 23);
+            this.SourceButton.TabIndex = 0;
+            this.SourceButton.Text = "Source";
+            this.SourceButton.UseVisualStyleBackColor = true;
+            this.SourceButton.Click += new System.EventHandler(this.SourceButton_Click);
+            // 
+            // DestinationButton
+            // 
+            this.DestinationButton.Location = new System.Drawing.Point(293, 21);
+            this.DestinationButton.Name = "DestinationButton";
+            this.DestinationButton.Size = new System.Drawing.Size(75, 23);
+            this.DestinationButton.TabIndex = 1;
+            this.DestinationButton.Text = "Destination";
+            this.DestinationButton.UseVisualStyleBackColor = true;
+            this.DestinationButton.Click += new System.EventHandler(this.DestinationButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 116);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(356, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // CopyButton
+            // 
+            this.CopyButton.Location = new System.Drawing.Point(152, 76);
+            this.CopyButton.Name = "CopyButton";
+            this.CopyButton.Size = new System.Drawing.Size(75, 23);
+            this.CopyButton.TabIndex = 3;
+            this.CopyButton.Text = "Copy!";
+            this.CopyButton.UseVisualStyleBackColor = true;
+            this.CopyButton.Click += new System.EventHandler(this.CopyButton_Click);
+            // 
+            // PowerCopyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 151);
+            this.Controls.Add(this.CopyButton);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.DestinationButton);
+            this.Controls.Add(this.SourceButton);
+            this.Name = "PowerCopyForm";
+            this.Text = "PowerCopy";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button SourceButton;
+        private System.Windows.Forms.Button DestinationButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button CopyButton;
+    }
+}
+
---
+++ PowerCopy/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PowerCopy
+{
+    public partial class PowerCopyForm : Form
+    {
+        DirectoryInfo sourceDirectory;
+        DirectoryInfo targetDirectory;
+
+        public PowerCopyForm()
+        {
+            InitializeComponent();
+        }
+
+        public void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                //Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+            }
+            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir = target.CreateSubdirectory(diSourceSubDir.Name);
+                CopyAll(diSourceSubDir, nextTargetSubDir);
+            }
+        }
+
+        private void SourceButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                sourceDirectory = new DirectoryInfo(folderBrowserDialog.SelectedPath);
+            }        
+        }
+
+        private void DestinationButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                targetDirectory = new DirectoryInfo(folderBrowserDialog.SelectedPath);
+            }
+        }
+
+        private void CopyButton_Click(object sender, EventArgs e)
+        {
+            if (sourceDirectory == null || targetDirectory == null)
+            {
+                MessageBox.Show("Please select a source and destination folder!");
+                return;                      
+            }
+            string source = sourceDirectory.FullName;
+            string target = targetDirectory.FullName;
+            DialogResult res = MessageBox.Show("Are you sure you want to Copy " + source + " folder to all folders in " + target, "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (res == DialogResult.Yes)
+            {
+                foreach (DirectoryInfo diTargetSubDir in targetDirectory.GetDirectories())
+                {                 
+                    DirectoryInfo subTargetDir = new DirectoryInfo(diTargetSubDir.FullName + "\\" + sourceDirectory.Name);
+                    CopyAll(sourceDirectory, subTargetDir);
+                }
+            }
+        }
+    }
+}
